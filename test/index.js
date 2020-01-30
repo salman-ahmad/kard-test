ccaeaae0ec3e58919ce11cf277268663dcc4f281
celcoder <celcoder@gmail.com>
2020-01-30 14:33:49 -0500

    ready for interview

--- test/index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-//Please write your code below this line.
-
-console.log('Hi There!')
---
+++ test/start.js
@@ -0,0 +1,5 @@
+'use strict';
+import transactions from './transactions.json';
+
+//Please write your code below this line.
+console.log(transactions);
